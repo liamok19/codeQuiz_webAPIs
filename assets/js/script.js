8d79652a4c411c1eeb22475de0feea53d87f41ce
Liam Ok <liamokane19@gmail.com>
2021-12-19 20:59:10 +1100

    updated initial click wvent for the game. Added variables and linked html ID's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,55 @@
-var startButton = document.getElementById('begin_btn');
-var questionBox = document.getElementById('questionBox');
+var startButtonEL = document.getElementById('begin_btn');
+var questionBoxEL = document.getElementById('questionBox');
+var highscoreEL = document.getElementById ('highscore');
+var timerEL = document.getElementById('timer');
 
-startButton.addEventListener('click', gameBegins);
+startButtonEL.addEventListener('click', gameBegins);
 
-function gameBegins (){
-    console.log('BEGIN Bitch');  
-    startButton.classList.add('invisible');
-    questionBox.classList.remove('invisible');
+
+
+var questions = [
+    {
+        question:"What is Pop Princess superstar Britney Spears middle name?",
+        options:["Jean", "Rose", "Jane", "Rosalyn"],
+        correct: 1, 
+    },
+    {
+        question:"What isn't a song sung by Britney Spears?",
+        options:["Toxic", "Circus", "Whoopsy Daisy", "Work Bitch"],
+        correct: 3, 
+    },
+];
+
+var currentQuestion = 0;
+
+var gameBegins = function(){
+
+    var timer = 60;
+
+    var timeInterval = setInterval(function() {
+        time--;
+        timeInterval.textcontent = time;
+
+        if (timer === 0 ) {
+            clearInterval(timeInterval);
+        }
+    },1000);
+
+    function showQuestion() {
+        var questionObject = questions[currentQuestion];
+            questionEl.innertext = questionObject.question;
+            
+
+    }
+
+
 }
 
-function nextQuestion (){
+// var timer. Must start at 60 and set to 10milliseconds
 
-}
+// function gameBegins (){}
+// function nextQuestion (){}
+// function answerSelection() {}
+// build variables. An object conatining an array of the questons 
 
-function answerSelection() {
 
-}
