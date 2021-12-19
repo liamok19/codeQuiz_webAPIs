236e7016373dae794b3ee4a902467aca19a2e541
Liam Ok <liamokane19@gmail.com>
2021-12-19 19:00:08 +1100

    created first question container with relevant buttons. CSS basic styling and begun adding JS functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-var question_01 = ()
+var startButton = document.getElementById('begin_btn');
+var questionBox = document.getElementById('questionBox');
+
+startButton.addEventListener('click', gameBegins);
+
+function gameBegins (){
+    console.log('BEGIN Bitch');  
+    startButton.classList.add('invisible');
+    questionBox.classList.remove('invisible');
+}
+
+function nextQuestion (){
+
+}
+
+function answerSelection() {
+
+}
