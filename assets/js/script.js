28bf8b165ed5cfb423e03f73f6c1e3b9b06f1e91
Liam Ok <liamokane19@gmail.com>
2021-12-20 17:51:45 +1100

    start button hides once click fucntion has been called. Pushing update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,52 +5,6 @@ var timerEl = document.getElementById('timer');
 var count = 61;
 
 
-var questions = [
-    {
-        question:"What is Pop Princess superstar Britney Spears middle name?",
-        options:["Jean", "Rose", "Jane", "Rosalyn"],
-        correct: 1, 
-    },
-    {
-        question:"What isn't a song sung by Britney Spears?",
-        options:["Toxic", "Circus", "Whoopsy Daisy", "Work Bitch"],
-        correct: 3, 
-    },
-    {
-        question:"What isn't a song sung by Britney Spears?",
-        options:["Toxic", "Circus", "Whoopsy Daisy", "Work Bitch"],
-        correct: 4, 
-    },
-    {
-        question:"What isn't a song sung by Britney Spears?",
-        options:["Toxic", "Circus", "Whoopsy Daisy", "Work Bitch"],
-        correct: 2, 
-    },
-];
-
-var currentQuestion = 0;
-
-function showQuestion() {
-
-    var questionObject = questions[currentQuestion];
-        questionBoxEl.innertext = questionObject.question;
-    var answer1El = document("Question_01");
-        answer1El.innertext = questionObject.answers[0];
-    var answer2El = document("Question_02");
-        answer2El.innertext = questionObject.answers[1];
-    var answer3El = document("Question_03");
-        answer3El.innertext = questionObject.answers[2];
-    var answer4El = document("Question_04");
-        answer4El.innertext = questionObject.answers[3];
-
-    choicesEl.appendChild(answer1El);
-    choicesEl.appendChild(answer2El);
-    choicesEl.appendChild(answer3El);
-    choicesEl.appendChild(answer4El);
-    
-
-}
-
 startButtonEl.addEventListener('click', function(){
     var timerInterval = setInterval(function(){
         count --;
@@ -61,7 +15,57 @@ startButtonEl.addEventListener('click', function(){
             clearInterval(timerInterval);
         }
     }, 1000);
+   var visible = startButtonEl.hidden = true;
+   if (visible === true) {
+    questionBoxEl.hidden = false;
+   }
 });
 
 
 
+
+// var questions = [
+//     {
+//         question:"What is Pop Princess superstar Britney Spears middle name?",
+//         options:["Jean", "Rose", "Jane", "Rosalyn"],
+//         correct: 1, 
+//     },
+//     {
+//         question:"What isn't a song sung by Britney Spears?",
+//         options:["Toxic", "Circus", "Whoopsy Daisy", "Work Bitch"],
+//         correct: 3, 
+//     },
+//     {
+//         question:"What isn't a song sung by Britney Spears?",
+//         options:["Toxic", "Circus", "Whoopsy Daisy", "Work Bitch"],
+//         correct: 4, 
+//     },
+//     {
+//         question:"What isn't a song sung by Britney Spears?",
+//         options:["Toxic", "Circus", "Whoopsy Daisy", "Work Bitch"],
+//         correct: 2, 
+//     },
+// ];
+
+// var currentQuestion = 0;
+
+// function showQuestion() {
+
+//     var questionObject = questions[currentQuestion];
+//         questionBoxEl.innertext = questionObject.question;
+//     var answer1El = document("Question_01");
+//         answer1El.innertext = questionObject.answers[0];
+//     var answer2El = document("Question_02");
+//         answer2El.innertext = questionObject.answers[1];
+//     var answer3El = document("Question_03");
+//         answer3El.innertext = questionObject.answers[2];
+//     var answer4El = document("Question_04");
+//         answer4El.innertext = questionObject.answers[3];
+
+//     choicesEl.appendChild(answer1El);
+//     choicesEl.appendChild(answer2El);
+//     choicesEl.appendChild(answer3El);
+//     choicesEl.appendChild(answer4El);
+    
+
+// }
